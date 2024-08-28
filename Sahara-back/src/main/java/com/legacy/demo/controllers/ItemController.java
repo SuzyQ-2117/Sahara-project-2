12fe679920eabe33507d0d6fe06bf9dc0cc2ae2c
SuzyQ <153325350+SuzyQ-2117@users.noreply.github.com>
2024-08-28 13:33:54 +0100

    updated create and add methods to include new attributes. New methods created for adding and removing multiple item tags

--- Sahara-back/src/main/java/com/legacy/demo/controllers/ItemController.java
+++ Sahara-back/src/main/java/com/legacy/demo/controllers/ItemController.java
@@ -17,32 +17,51 @@ public class ItemController {
 
     public ItemController(ItemService service) { this.service = service;}
 
+    //CREATE
+    @PostMapping("/item/add")
+    public ResponseEntity<?> addItem(@RequestBody Item item){
+        return this.service.addItem(item);
+    }
+
+    //READ
     @GetMapping("/items/getAll")
     public List<ItemDto> getAll() {return this.service.getAll();}
 
     @GetMapping("/items/get/{id}")
     public ResponseEntity<?> getItem(@PathVariable Integer id) {return this.service.getItem(id);}
 
-    @PostMapping("/item/add")
-    public ResponseEntity<?> addItem(@RequestBody Item item){
-        return this.service.addItem(item);
-    }
-
-    @DeleteMapping("/item/remove/{id}")
-    public ResponseEntity<?> removeItem(@PathVariable Integer id){
-        return this.service.removeItem(id);
-    }
-
-    @PatchMapping("item/update/{id}")
-    public ResponseEntity<?> updateItem(@PathVariable Integer id,
-                                        @RequestBody Item ItemUpdate){
-        return this.service.ItemUpdate(id, ItemUpdate.getName(), ItemUpdate.getPrice(), ItemUpdate.getQuantity(), ItemUpdate.getImageUrl());
-    }
-
     @PostMapping("/items/getByIds")
     public List<ItemDto> getItemsByIds(@RequestBody List<Integer> ids) {
         return this.service.getItemsByIds(ids);
     }
 
+    //UPDATE
+    @PatchMapping("item/update/{id}")
+    public ResponseEntity<?> updateItem(@PathVariable Integer id,
+                                        @RequestBody Item ItemUpdate){
+        return this.service.ItemUpdate(id, ItemUpdate.getName(), ItemUpdate.getPrice(), ItemUpdate.getQuantity(), ItemUpdate.getImageUrl(), ItemUpdate.getColor(), ItemUpdate.getCategory(), ItemUpdate.getStockAvailable());
+    }
+
+    //UPDATE - add tag
+    @PatchMapping("/item/update/{id}/addTag")
+    public ResponseEntity<ItemDto> addTag(@PathVariable Integer id,
+                                    @RequestBody List<String> tagsToAdd){
+        Item updatedItem = this.service.addTags(id, tagsToAdd);
+        return ResponseEntity.ok(new ItemDto(updatedItem));
+    }
+
+    //UPDATE - remove tag
+    @PatchMapping("item/update/{id}/removeTag")
+    public ResponseEntity<ItemDto> removeTag(@PathVariable Integer id,
+                                       @RequestBody List<String> tagsToRemove){
+        Item updatedItem = this.service.removeTags(id, tagsToRemove);
+        return ResponseEntity.ok(new ItemDto(updatedItem));
+    }
+
+    //DELETE
+    @DeleteMapping("/item/remove/{id}")
+    public ResponseEntity<?> removeItem(@PathVariable Integer id){
+        return this.service.removeItem(id);
+    }
 
 }
--- Sahara-back/src/main/java/com/legacy/demo/services/ItemService.java
+++ Sahara-back/src/main/java/com/legacy/demo/services/ItemService.java
@@ -4,6 +4,7 @@ import com.legacy.demo.repos.ItemRepo;
 import com.legacy.demo.entities.Item;
 import com.legacy.demo.dtos.ItemDto;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,14 @@ public class ItemService {
         this.repo = repo;
     }
 
+    //CREATE
+    public ResponseEntity<ItemDto> addItem(Item newItem) {
+        Item created = this.repo.save(newItem);
+
+        return new ResponseEntity<>(new ItemDto(created), HttpStatus.CREATED);
+    }
+
+    //READ
     public List<ItemDto> getAll() {
         List<ItemDto> dtos = new ArrayList<>();
         List<Item> found = this.repo.findAll();
@@ -39,50 +48,6 @@ public class ItemService {
         return ResponseEntity.ok(new ItemDto(found.get()));
     }
 
-    public ResponseEntity<ItemDto> addItem(Item newItem) {
-        Item created = this.repo.save(newItem);
-
-        return new ResponseEntity<>(new ItemDto(created), HttpStatus.CREATED);
-    }
-
-    public ResponseEntity<?> removeItem(Integer id) {
-        Optional<Item> found = this.repo.findById(id);
-        if (found.isEmpty()) {
-            return new ResponseEntity<>("No Item found with id " + id, HttpStatus.NOT_FOUND);
-        }
-        this.repo.deleteById(id);
-        return ResponseEntity.ok("Item with id " + id + " has been deleted.");
-
-    }
-
-    public ResponseEntity<?> ItemUpdate(Integer id,
-                                            String name,
-                                            Double price,
-                                            Integer quantity,
-                                        String imageUrl){
-
-        Optional<Item> found = this.repo.findById(Math.toIntExact(id));
-        if (found.isEmpty()) {
-            return new ResponseEntity<>("No Item found with ID " + id, HttpStatus.NOT_FOUND);
-        }
-
-        Item toUpdate = found.get();
-
-
-        if (name != null) toUpdate.setName(name);
-        if (price != null) toUpdate.setPrice(price);
-        if (quantity != null) toUpdate.setQuantity(quantity);
-        if (imageUrl != null) toUpdate.setImageUrl(imageUrl);
-
-        Item updated = this.repo.save(toUpdate);
-        return ResponseEntity.ok(new ItemDto(updated));
-    }
-
-
-
-
-
-
     public List<ItemDto> getItemsByIds(List<Integer> ids) {
         List<Item> items = this.repo.findAllById(ids);
         List<ItemDto> dtos = new ArrayList<>();
@@ -92,6 +57,80 @@ public class ItemService {
         return dtos;
     }
 
+    //UPDATE
+    public ResponseEntity<?> ItemUpdate(Integer id,
+                                        String name,
+                                        Double price,
+                                        Integer quantity,
+                                        String imageUrl,
+                                        String color,
+                                        String category,
+                                        Boolean stockAvailable){
+
+        Optional<Item> found = this.repo.findById(Math.toIntExact(id));
+        if (found.isEmpty()) {
+            return new ResponseEntity<>("No Item found with ID " + id, HttpStatus.NOT_FOUND);
+        }
+
+        Item toUpdate = found.get();
+
+        if (name != null) toUpdate.setName(name);
+        if (price != null) toUpdate.setPrice(price);
+        if (quantity != null) toUpdate.setQuantity(quantity);
+        if (imageUrl != null) toUpdate.setImageUrl(imageUrl);
+        if (color != null) toUpdate.setColor(color);
+        if (category != null) toUpdate.setCategory(category);
+        if (stockAvailable != null) toUpdate.setStockAvailable(stockAvailable);
+
+        Item updated = this.repo.save(toUpdate);
+        return ResponseEntity.ok(new ItemDto(updated));
+    }
+
+    //UPDATE - add tag(s) to item
+    public Item addTags(Integer id, List<String> tagsToAdd) {
+        Optional<Item> found = this.repo.findById(id);
+        if (found.isPresent()) {
+            Item item = found.get();
+            ArrayList<String> currentTags = item.getTags();
+            for (String tag : tagsToAdd) {
+                if (!currentTags.contains(tag)) {
+                    currentTags.add(tag);
+                }
+            }
+            item.setTags(currentTags);
+            this.repo.save(item);
+            return item;
+        } else {
+            throw new EntityNotFoundException("Item not found with ID: " + id);
+        }
+    }
 
 
+    //UPDATE - remove tag(s) from item
+    public Item removeTags(Integer id, List<String> tagsToRemove) {
+        Optional<Item> found = this.repo.findById(id);
+        if (found.isPresent()) {
+            Item item = found.get();
+            ArrayList<String> currentTags = item.getTags();
+            currentTags.removeAll(tagsToRemove);
+            item.setTags(currentTags);
+            this.repo.save(item);
+            return item;
+        } else {
+            throw new EntityNotFoundException("Item not found with ID: " + id);
+        }
+    }
+
+
+
+    //DELETE
+    public ResponseEntity<?> removeItem(Integer id) {
+        Optional<Item> found = this.repo.findById(id);
+        if (found.isEmpty()) {
+            return new ResponseEntity<>("No Item found with id " + id, HttpStatus.NOT_FOUND);
+        }
+        this.repo.deleteById(id);
+        return ResponseEntity.ok("Item with id " + id + " has been deleted.");
+
+    }
 }
